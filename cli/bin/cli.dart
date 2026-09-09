f3ffebffa840acf40e77242a5380d5dd982c1503
antoniosnt <antoniosnt.dev@gmail.com>
2026-09-09 11:25:32 -0300

    dart-2026: add interactivity to your app ✅

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -1,16 +1,18 @@
 import 'package:cli/cli.dart' as cli;
 
-const version = "0.0.1";
+const String version = '0.0.1';
 
 /// dart run (without arguments)
 /// dart bin/cli.dart version (with arguments)
 void main(List<String> arguments) {
-  if (arguments.isEmpty || arguments.first == "help") {
+  if (arguments.isEmpty || arguments.first == 'help') {
     cli.printUsage();
-  } else if (arguments.first == "version") {
-    print("Dartpedia CLI version $version");
+  } else if (arguments.first == 'version') {
+    print('Dartpedia CLI version $version');
   } else if (arguments.first == 'search') {
-    print("Search command recognized!");
+    /// Final variables is used when you never intend to change the variable again.
+    final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
+    cli.searchWikipedia(inputArgs);
   } else {
     cli.printUsage();
   }
--- cli/lib/cli.dart
+++ cli/lib/cli.dart
@@ -1,5 +1,23 @@
+import 'dart:io';
+
 void printUsage() {
   print(
-    "The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE'",
+    'The following commands are valid: "help", "version", "search <ARTICLE-TITLE"',
   );
+}
+
+/// Added the "?" to arguments type because the argument can be null (null safety).
+void searchWikipedia(List<String>? arguments) {
+  final String articleTitle;
+
+  if (arguments == null || arguments.isEmpty) {
+    print('Please provide an article title.');
+    articleTitle = stdin.readLineSync() ?? '';
+  } else {
+    articleTitle = arguments.join(' ');
+  }
+
+  print('Looking up articles about "$articleTitle". Please wait.');
+  print('Here ya go!');
+  print('(Pretend this is an article about "$articleTitle")');
 }
