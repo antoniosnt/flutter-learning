70a67c4d2d40334aa1795a58360f797748494663
antoniosnt <antoniosnt.dev@gmail.com>
2026-09-10 16:09:26 -0300

    flutter: Introduction to Flutter UI -> Widget Fundamentals 01 - 04 ✅

---
+++ birdle/lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:birdle/game.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(body: Center(child: Row(children: [Tile('A', HitType.hit), Tile('B', HitType.miss), Tile('C', HitType.partial)],))),
+    );
+  }
+}
+
+class Tile extends StatelessWidget {
+  const Tile(this.letter, this.hitType, {super.key});
+
+  final String letter;
+  final HitType hitType;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Replace Container with widgets
+    return Container(
+      width: 60,
+      height: 60,
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey.shade300),
+        color: switch (hitType) {
+          HitType.hit => Colors.green,
+          HitType.partial => Colors.yellow,
+          HitType.miss => Colors.grey,
+          _ => Colors.white,
+        },
+      ),
+      child: Center(
+        child: Text(
+          letter.toUpperCase(),
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
+      ),
+    );
+  }
+}
